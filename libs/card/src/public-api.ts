c3ba86100779478da82626255d9cf7f73916849a
Quentin <quentin@halfsquare.fr>
2025-01-17 12:05:54 +0100

    Updates

--- libs/card/src/public-api.ts
+++
@@ -1,5 +0,0 @@
-/*
- * Public API Surface of card
- */
-
-export * from './lib/card.component';
--- libs/carousel/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of carousel
- */
-
-export * from './lib/slide.component';
-export * from './lib/carousel.component';
